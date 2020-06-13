6d161bd6bd22751240b44247a96deba228e94be9
kato hawkins <kristhahawkins@icloud.com>
2020-06-12 21:48:52 -0400

    finished closures and prototype

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,19 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
+//because the variable is on the global scope 
 
 
 /* Task 2: Counter */
 
-/* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+/* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. 
+For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(Count){
+  let counter = 0;
+  for (let i = 0; i <=Count; i++){
+  counter+=i;
+  }
+  return counter;
+
+}
+console.log(summation(4))
